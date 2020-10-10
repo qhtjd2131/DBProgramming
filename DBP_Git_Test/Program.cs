0b06a3ea09c83624e252b3f5df965a9077aa0f0c
BoSung Choi <qhtjd2131@gmail.com>
2020-10-10 19:01:59 +0900

    DBP_GIT_TEST v0.1 프로젝트생성

---
+++ DBP_Git_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBP_Git_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 5;
+            Console.WriteLine("hello world");
+            Console.WriteLine(a + b);
+        }
+    }
+}
